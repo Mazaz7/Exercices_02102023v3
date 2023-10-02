0b220d3ea3304821ce7ec51dd173833dcdf82212
Samy Mazazi <mazazi.samy@gmail.com>
2023-10-02 14:33:40 +0200

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests
 try:
-    # Make an API request to the Dad Jokes API
     response = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
 
-    # Check if the response status code indicates success (200 OK)
     if response.status_code == 200:
         # Parse the JSON response to retrieve the joke
         data = response.json()
@@ -12,8 +10,8 @@ try:
     else:
         print(f"API request failed with status code: {response.status_code}")
 except requests.exceptions.RequestException as e:
-    # Handle network-related exceptions (e.g., no internet connection)
     print(f"An error occurred: {e}")
 except Exception as e:
-    # Handle other exceptions
     print(f"An unexpected error occurred: {e}")
+"What is this change"
+"So much changes"
